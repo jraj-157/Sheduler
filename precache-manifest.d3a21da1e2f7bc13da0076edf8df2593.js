ee9a60ee502801d691f41ec44100813871f7be5a
jraj patnaik <96097449+jraj-157@users.noreply.github.com>
2022-06-05 15:21:49 +0530

    Updates

---
+++ precache-manifest.d3a21da1e2f7bc13da0076edf8df2593.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ea0a22e3a2ec05e279dc3134c87b97",
+    "url": "/Sheduler/index.html"
+  },
+  {
+    "revision": "87e9f199d8c24c01dd87",
+    "url": "/Sheduler/static/js/2.bd852f2c.chunk.js"
+  },
+  {
+    "revision": "08057abc969f1499c5d0",
+    "url": "/Sheduler/static/js/main.0f54d7c9.chunk.js"
+  },
+  {
+    "revision": "67ddb810452f0e4e0403",
+    "url": "/Sheduler/static/js/runtime-main.41309e1d.js"
+  }
+]);
